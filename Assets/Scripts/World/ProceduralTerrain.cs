3e7ddc9de3f0bce6d70532a37ec36e55572f52e2
HAKFOO <wjyfete@gmail.com>
2025-10-17 08:41:57 +0800

    地形生成思路问题

---
+++ Assets/Scripts/World/ProceduralTerrain.cs
@@ -0,0 +1,145 @@
+using System.Collections.Generic;
+using MineCraft;
+using UnityEngine;
+
+/// <summary>
+/// 世界生成
+/// </summary>
+public class ProceduralTerrain : MonoBehaviour
+{
+    /// <summary>
+    /// 噪声缩放
+    /// </summary>
+    [SerializeField] private float noiseScale = 0.05f;
+    /// <summary>
+    /// 世界尺寸
+    /// </summary>
+    [SerializeField] private int worldSize = 6;
+    /// <summary>
+    /// 世界高度
+    /// </summary>
+    [SerializeField] private int worldHeight = 10;
+    /// <summary>
+    /// 水面高度
+    /// </summary>
+    [SerializeField] private int waterHeight = 4;
+
+    /// <summary>
+    /// 保存每个BLock类型的颜色
+    /// </summary>
+    private Dictionary<t_BlockType, Color> colorDic = new Dictionary<t_BlockType, Color>();
+
+    /// <summary>
+    /// 方块数据缓存
+    /// </summary>
+    private List<t_BlockData> worldBlockData = new List<t_BlockData>();
+
+    #region event function
+    private void Awake()
+    {
+        //将Block的材质和颜色一一对应
+        colorDic.Add(t_BlockType.Grass_Dirt, Color.green);//草方块对应绿色
+        colorDic.Add(t_BlockType.Dirt, Color.yellow);//泥土对应黄色
+        colorDic.Add(t_BlockType.Water, Color.blue);//水对应蓝色
+        colorDic.Add(t_BlockType.Sand, Color.gray);//沙对应灰色
+    }
+
+    private void Start()
+    {
+        GenerateTerrainData();
+    }
+    #endregion
+
+    #region FUNCTION
+
+    /// <summary>
+    /// 生成地图数据求出每个坐标的高度值
+    /// </summary>
+    private void GenerateTerrainData()
+    {
+        t_BlockType blockType;
+        for (int i = 0; i < worldSize; i++)
+        {
+            for (int j = 0; j < worldSize; j++)
+            {
+                //柏林噪声取 用水平坐标*噪声缩放 取值
+                float noiseValue = Mathf.PerlinNoise((transform.position.x + i) * noiseScale, (transform.position.z + j) * noiseScale);
+
+                int groundPostion = Mathf.RoundToInt(noiseValue * worldSize);
+
+                for (int y = 0; y < worldHeight; y++)
+                {
+                    blockType = t_BlockType.Dirt;
+                    if (y > groundPostion)
+                    {
+                        if (y < waterHeight)
+                        {
+                            blockType = t_BlockType.Water;
+                        }
+                        else
+                        {
+                            blockType = t_BlockType.Air;
+                        }
+                    }
+                    else if (y == groundPostion && y < waterHeight)
+                    {
+                        blockType = t_BlockType.Sand;
+                    }
+                    else if (y == groundPostion)
+                    {
+                        blockType = t_BlockType.Grass_Dirt;
+                    }
+
+                    t_BlockData temp = new t_BlockData(new Vector3(transform.position.x + i, y, transform.position.z + j), blockType);
+                    worldBlockData.Add(temp);
+                }
+
+            }
+        }
+    }
+
+    /// <summary>
+    /// 绘制地形预览
+    /// </summary>
+    private void OnDrawGizmos()
+    {
+        if (worldBlockData.Count > 0)
+        {
+            foreach (var block in worldBlockData)
+            {
+                if (block.blockType == t_BlockType.Air)
+                {
+                    continue;
+                }
+                Gizmos.color = colorDic[block.blockType];
+                Gizmos.DrawCube(block.blockPosition, new Vector3(1f, 1f, 1f));
+            }
+        }
+
+    }
+
+    #endregion
+
+    #region DATA
+
+    internal class t_BlockData
+    {
+        public Vector3 blockPosition;
+        public t_BlockType blockType;
+        public t_BlockData(Vector3 postion, t_BlockType type)
+        {
+            blockPosition = postion;
+            blockType = type;
+        }
+    }
+    public enum t_BlockType
+    {
+        None,
+        Air,
+        Grass_Dirt,
+        Dirt,
+        Water,
+        Sand,
+    }
+    #endregion
+}
--- README.md
+++ README.md
@@ -1,8 +1,56 @@
-# MineCraftForUnity
-用Unity实现我的世界
+<div align="center">
+  <img src="https://upload.wikimedia.org/wikipedia/zh/1/17/Minecraft_explore_landscape.png" alt="">
+  <h1>MineCraftForUnity</h1>
+  <blockquote>unity + MineCraft </blockquote>
+  <blockquote>用Unity实现我的世界 </blockquote>
+</div>
 
----
+
+
+## 地图生成
+
+- https://zhuanlan.zhihu.com/p/571622819
+
+按照文章的内容对生成的单个区块的地形生成做了实现，大致思路如下：
+
+1. 生成噪声
+2. 噪声值与高度的乘积决定上表面
+3. 与最高上表面和水面高度进行判断
+   1. 最高表面为`Grass_Dirt`，其余下面为`Dirt`
+   2. 高于最高表面的取最大值
+   3. 低于水面的上表面材质换成`sand`，高于的正常材质
 
 
 
-![](https://upload.wikimedia.org/wikipedia/zh/1/17/Minecraft_explore_landscape.png)
+> `Sand`:灰色、`Grass_Dirt`:绿色、`Dirt`:黄色、`Water`:蓝色
+
+![image-20251017082040415](E:\Unity\Unity Study\MineCraftForUnity\README.assets\image-20251017082040415.png)
+
+虽然模拟实现了地形区块生成的过程，但是地形过大的时候会导致游戏性能开销极大，因此方法只能作为一个最初的思路（也许还有其他方向？）。
+
+
+
+## 方块编辑器
+
+
+
+## 功能清单
+
+- [ ] 地形生成
+- [ ] 方块编辑
+- [ ] 人物控制
+- [ ] 工具使用
+  - [ ] 稿子
+  - [ ] 剑
+  - [ ] 斧头
+- [ ] 多人联机
+
+
+
+## 参考资料
+
+- https://developer.unity.cn/projects/5f35326fedbc2a002071984d
+  - https://github.com/Jin-Yuhan/MinecraftClone-Unity
+- https://zhuanlan.zhihu.com/p/571622819
+
+- 【【Unity教程搬运】如何在Unity中创建Minecraft：终极指南 (2024)】 https://www.bilibili.com/video/BV1cJ4m137oj/?share_source=copy_web&vd_source=03b89d7c0f287bbb21e7897d1e86a944
